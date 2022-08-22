7d8010e602f8697f8c4982bb324f234a528509a4
GuillaumeVDV <vdvguillaume@gmail.com>
2022-08-22 17:38:59 +0200

    Cleanup css & js files

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -32,18 +32,4 @@ $(document).ready(function() {
         return false;
     });
 
-
-
-    // 3. welcome animation support
-
-    $(window).load(function() {
-        $(".welcome-hero-txt h4,.welcome-hero-txt h2,.welcome-hero-txt p").removeClass("animated fadeInUp").css({ 'opacity': '0' });
-        $(".welcome-hero-txt button").removeClass("animated fadeInDown").css({ 'opacity': '0' });
-    });
-
-    $(window).load(function() {
-        $(".welcome-hero-txt h4,.welcome-hero-txt h2,.welcome-hero-txt p").addClass("animated fadeInUp").css({ 'opacity': '0' });
-        $(".welcome-hero-txt button").addClass("animated fadeInDown").css({ 'opacity': '0' });
-    });
-
 });
